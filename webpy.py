6ab2bad16902c8d593ac73390da9ef6bc82ab0ab
Michał Derkacz <michal@Lnet.pl>
2011-02-24 23:33:51 +0100

    Added support for web.py+fapws3

--- webpy.py
+++ webpy.py
@@ -2,6 +2,7 @@
 from sys import argv
 import web
 
+
 class ServeHTTP(object):
     def GET(self, txt, num):
         try:
@@ -17,7 +18,11 @@ urls = ("^/([^/]+)/([^/]+)$", "ServeHTTP")
 app = web.application(urls, globals())
 wsgifunc = app.wsgifunc()
 
+
 if __name__ == "__main__":
+    def usage():
+        print "Usage: %s [{cherrypy|gevent|fapws3} PORT]" % argv[0]
+
     if len(argv) > 2:
         sname = argv[1]
         port = int(argv[2])
@@ -33,12 +38,24 @@ if __name__ == "__main__":
             from gevent.wsgi import WSGIServer
             server = WSGIServer(("0.0.0.0", port), wsgifunc, log=None)
             server.serve_forever()
+        elif sname == "fapws3":
+            import fapws._evwsgi as evwsgi
+            from fapws import base
+            evwsgi.start("0.0.0.0", str(port))
+            evwsgi.set_base_module(base)
+            evwsgi.wsgi_cb(("", wsgifunc))
+            evwsgi.set_debug(0)
+            evwsgi.run()
         else:
-            print "Unknown server"
-    else:
+            usage()
+    elif len(argv) == 0:
         from flup.server.fcgi import WSGIServer
         server = WSGIServer(wsgifunc, bindAddress=None,
                             multiplexed=False, multithreaded=False)
         server.run()
         #web.wsgi.runwsgi = lambda func, addr=None: web.wsgi.runfcgi(func, addr)
         #app.run()
+    else:
+        usage()
+
+
